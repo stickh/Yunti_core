e041d1a5caf017667207d13a512e4357e1202d48
Shelikhoo <xiaokangwang@outlook.com>
2015-07-21 10:02:33 +0800

    crossing template

---
+++ crossingtransport.go
@@ -0,0 +1,24 @@
+package yunti_core
+
+import net
+
+type YuntiProxyConn struct{}
+
+func(c YuntiProxyConn)Close(){
+  //TODO
+}
+
+func(c YuntiProxyConn)Write(b []byte) (n int, err error){
+  //TODO
+}
+
+func(c YuntiProxyConn)Read(b []byte) (n int, err error){
+  //TODO
+}
+
+func ProxyDialer(network, address string) (net.Conn, error){
+
+  var conninsT YuntiProxyConn
+
+  return conninsT
+}
